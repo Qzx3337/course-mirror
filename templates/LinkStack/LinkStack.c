65e5af85f9cd0f6afc7905062209623f7ccd2746
Qzx <Henry3337@hotmail.com>
2023-04-14 21:28:55 +0800

    优化LINKSTACH.H模板类

--- templates/LinkStack/LinkStack.c
+++ templates/LinkStack/LinkStack.c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "LinkStack.h"
 
 int main() {
--- templates/LinkStack/LinkStack.h
+++ templates/LinkStack/LinkStack.h
@@ -1,70 +1,73 @@
 /*
-* @Description: 链式栈的接口
+链式栈的接口
 */
 
 #pragma once
 
-#include <stdlib.h>
-#define true 1
-#define false 0
-
-#define mcreate(type, n) ((type)*)malloc((n)*sizeof((type)))
-#define mcreate(type) ((type)*)malloc(sizeof((type)))
-
 #ifndef StackElemType
 #define StackElemType int
 #endif // !StackElemType
+#ifndef OVERFLOW
+#define OVERFLOW 3
+#endif // !OVERFLOW
+
 
 typedef struct LinkStackNode {
 	StackElemType data;
 	struct LinkStackNode* next;
 } LinkStackNode;
 
-typedef struct LinkStackBody{
+typedef struct _LinkStack {
 	int size;
 	LinkStackNode* top;
-} LinkStackBody,*Stack;
+} _LinkStack, * Stack;
 
 
 /******** 只读接口 **********/
 
-int isEmpty(Stack s);
-int getStaSize(Stack s);
-StackElemType getTop(Stack s);
+bool isEmpty(Stack const s);
+int getStaSize(Stack const s);
+StackElemType getTop(Stack const s);
 
 /******** 可写接口 ************/
 
-int initSta(Stack* s);
-int popSta(Stack s);
-int pushSta(Stack s, StackElemType data);
+Stack newStack();
+bool popSta(Stack sta);
+void pushSta(Stack sta, StackElemType const data);
 
 
 /******** 实现  **************/
 
-int isEmpty(Stack s) {
+bool isEmpty(Stack s)
+{
 	return s->size == 0;
 }
 
-inline int getStaSize(Stack s) {
+inline int getStaSize(Stack s)
+{
 	return s->size;
 }
 
-StackElemType getTop(Stack s) {
+StackElemType getTop(Stack s)
+{
 	return s->top->next->data;
 }
 
-// 初始化栈
-int initSta(Stack* sta) {
-	(*sta) = (Stack)malloc(sizeof(LinkStackBody));
-	if (!(*sta)) return false;				// 分配失败
-	(*sta)->size = 0;
-	(*sta)->top = (LinkStackNode*)malloc(sizeof(LinkStackNode));
-	(*sta)->top->next = NULL;
-	return true;
+// 新建栈
+Stack newStack()
+{
+	Stack sta = (Stack)malloc(sizeof(_LinkStack));
+	if (!sta) exit(OVERFLOW);				// 分配失败
+	sta->size = 0;
+	sta->top = (LinkStackNode*)malloc(sizeof(LinkStackNode));
+	if (sta->top == NULL) exit(OVERFLOW);
+	sta->top->next = NULL;
+	return sta;
 }
 
 // 出栈，成功返回 1 ，失败返回 0 
-inline int popSta(Stack sta) {
+inline bool popSta(Stack sta)
+{
 	if (sta->size == 0) return false;
 
 	//EraseNext(sta->top);
@@ -77,9 +80,10 @@ inline int popSta(Stack sta) {
 }
 
 // 压栈
-inline int pushSta(Stack sta, StackElemType data) {
+inline void pushSta(Stack sta, StackElemType const data)
+{
 	LinkStackNode* newNode = (LinkStackNode*)malloc(sizeof(LinkStackNode));
-	if (!newNode) return false;			// 分配失败
+	if (newNode == NULL) exit(OVERFLOW);			// 分配失败
 	newNode->data = data;
 
 	//InsertAsNext(sta->top, newNode);
@@ -87,5 +91,4 @@ inline int pushSta(Stack sta, StackElemType data) {
 	sta->top->next = newNode;
 
 	sta->size++;
-	return true;
 }
