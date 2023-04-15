7ee925e97b0dd7f20182beb39d1b37b392022e49
Qzx <Henry3337@hotmail.com>
2023-04-15 18:15:43 +0800

    建树，正常

--- Lecture4/Problem5/p5.c
+++ Lecture4/Problem5/p5.c
@@ -9,7 +9,7 @@ language: C.
 
 int main() {
 
-	//freopen("p5.in", "r", stdin);
+	freopen("p5.in", "r", stdin);
 	//freopen("p5.out", "w", stdout);
 
 	char posSeq[MAXLEN];
@@ -17,12 +17,12 @@ int main() {
 
 	BinTree tree = createBinTree_Pre(posSeq);	// 先序创建树结点
 
-	//travPre(tree, printTreeNode);
-	//printf("\n");
+	travPre(tree, printTreeNode);
+	printf("\n");
 	//travIn(tree, printTreeNode);
 	//printf("\n");
-	travPost(tree, printTreeNode);
-	printf("\n");
+	//travPost(tree, printTreeNode);
+	//printf("\n");
 	//travLevel(tree, printTreeNode);
 	//printf("\n");
 
