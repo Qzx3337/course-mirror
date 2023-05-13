bd2ae801fdde3c8de6fea265cf03d559eaf85275
Qzx <Henry3337@hotmail.com>
2023-05-13 15:31:09 +0800

    p0503,AC

---
+++ Lecture5/p0503/p0503.c
@@ -0,0 +1,103 @@
+/*
+3、哈夫曼编码
+读入n个字符所对应的权值，构造一棵哈夫曼树，自底向上生成每一个字符对应的哈夫曼编码，并依次输出。
+language: C.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#define INF 0x7FFFFFFF
+#define MAXN 20				// 最大字符数
+#define MAXM (2 * MAXN - 1)		// 最大结点数
+
+// 哈夫曼树结点结构体
+typedef struct HTNode
+{
+	int weight;                 // 结点的权值
+	int father;                 // 双亲的下标
+	int LChild;                 // 左孩子结点的下标
+	int RChild;                 // 右孩子结点的下标
+} HTNode;
+
+int n;		// 字符数
+HTNode HuffmanTree[MAXM + 1];		// 哈夫曼树
+
+int main() {
+
+	//freopen("p0503.in", "r", stdin);
+	//freopen("p0503.out", "w", stdout);
+
+	// 读入
+
+	int i, j, k;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &HuffmanTree[i].weight);
+		HuffmanTree[i].father = 0;
+		HuffmanTree[i].LChild = 0;
+		HuffmanTree[i].RChild = 0;
+	}
+
+	// 构造哈夫曼树
+
+	int min1, min2;		// 最小的两个权值
+	int x1, x2;			// 最小的两个权值结点的下标
+	for (i = n + 1; i <= 2 * n - 1; i++) {
+		min1 = min2 = INF;
+		x1 = x2 = 0;
+		for (j = 1; j < i; j++) {
+			if (HuffmanTree[j].father == 0) {
+				if (HuffmanTree[j].weight < min1) {
+					min2 = min1;
+					x2 = x1;
+					min1 = HuffmanTree[j].weight;
+					x1 = j;
+				}
+				else if (HuffmanTree[j].weight < min2) {
+					min2 = HuffmanTree[j].weight;
+					x2 = j;
+				}
+			}
+		}
+		HuffmanTree[x1].father = i;
+		HuffmanTree[x2].father = i;
+		HuffmanTree[i].LChild = x1;
+		HuffmanTree[i].RChild = x2;
+		HuffmanTree[i].weight = min1 + min2;
+	}
+
+	// 输出
+
+	char code[MAXN + 1];
+    code[n] = '\0';
+	for (i = 1; i <= n; i++) {
+		j = i;
+		k = n - 1;
+		while (HuffmanTree[j].father != 0) {
+			if (HuffmanTree[HuffmanTree[j].father].LChild == j) {
+				code[k] = '0';
+			}
+			else if(HuffmanTree[HuffmanTree[j].father].RChild == j){
+				code[k] = '1';
+			}
+			else {
+				assert(false);
+			}
+			k--;
+			j = HuffmanTree[j].father;
+		}
+		printf("%s\n", code + k + 1);
+	}
+
+
+	return 0;
+}
+
