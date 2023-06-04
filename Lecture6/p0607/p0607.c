7d4ab010637e99f5704dab89ccec48e3975af859
Qzx <Henry3337@hotmail.com>
2023-06-04 18:15:56 +0800

    创建p0607模板

---
+++ Lecture6/p0607/p0607.c
@@ -0,0 +1,23 @@
+/***
+language: C.
+
+***/
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <assert.h>
+
+int main(int argc, char const **argv)
+{
+	//freopen("p0607.in", "r", stdin);
+	//freopen("p0607.out", "w", stdout);
+
+
+	return 0;
+}
+
